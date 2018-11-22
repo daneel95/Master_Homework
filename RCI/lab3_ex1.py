8495d9956fab1b4a8896c1e604f24f35e6548186
Daniel Holteiu <holteiu@Daniels-MacBook-Pro.local>
2018-11-22 21:40:52 +0200

    RCI Tema 1 lab2 + lab3 ex1

--- RCI/lab3_ex1.py
+++ RCI/lab3_ex1.py
@@ -8,6 +8,12 @@ def create_not(el):
     return '{}{}{}'.format('n(', el, ')')
 
 
+def opposite(el):
+    if el[0:2] == 'n(':
+        return break_not(el)
+    return create_not(el)
+
+
 def from_string_to_array(input):
     list_to_return = []
     input = input[1:-1]
@@ -18,6 +24,92 @@ def from_string_to_array(input):
         list_to_return.append(my_list)
         input = input[close + 1:]
     return list_to_return
+
+
+# def try_to_apply2(my_list):
+#     my_list = [sorted(el) for el in my_list]
+#     doable = False
+#     indexes_to_eliminate = []
+#     new_rezolvant = []
+#     aux = 0
+#     for rezolvant, index in zip(my_list, range(len(my_list))):
+#         for rezolvant_next, index2 in zip(my_list[index + 1:], range(index + 1, len(my_list))):
+#             for el in rezolvant:
+#                 if opposite(el) in rezolvant_next:
+#                     print(el)
+#                     print(opposite(el))
+#                     copy_rezolvant = rezolvant[:]
+#                     copy_rezolvant.pop(copy_rezolvant.index(el))
+#                     copy_rezolvant_next = rezolvant_next[:]
+#                     copy_rezolvant_next.pop(copy_rezolvant_next.index(opposite(el)))
+#                     new_rezolvant = [sorted(list(set(copy_rezolvant + copy_rezolvant_next)))]
+#                     if opposite(el) in rezolvant and el in rezolvant_next:
+#                         copy_rezolvant = rezolvant[:]
+#                         copy_rezolvant.pop(copy_rezolvant.index(opposite(el)))
+#                         copy_rezolvant_next = rezolvant_next[:]
+#                         copy_rezolvant_next.pop(copy_rezolvant_next.index(el))
+#                         new_rezolvant.append(sorted(list(set(copy_rezolvant + copy_rezolvant_next))))
+#                     # aux_list = my_list[:]
+#                     # aux_list.pop(index2)
+#                     # aux_list.pop(index)
+#                     print(new_rezolvant)
+#                     yes = True
+#                     for nr in new_rezolvant:
+#                         if nr in my_list:
+#                             yes = False
+#                             break
+#                     if yes:
+#                         aux+=1
+#                         indexes_to_eliminate = [index, index2]
+#                         doable = True
+#                         break
+#             if doable:
+#                 break
+#         if doable:
+#             break
+#     print(str(aux))
+#     if indexes_to_eliminate:
+#         print("-----------------")
+#         print(my_list)
+#         my_list.pop(indexes_to_eliminate[1])
+#         my_list.pop(indexes_to_eliminate[0])
+#         print(my_list)
+#         my_list = my_list + new_rezolvant
+#         print(my_list)
+#         print("---------------")
+#         doable = True
+#     return doable, my_list
+
+
+
+def try_to_apply(my_list):
+    my_list = [sorted(el) for el in my_list]
+    doable = False
+    indexes_to_eliminate = []
+    new_rezolvant = []
+    for rezolvant, index in zip(my_list, range(len(my_list))):
+        for rezolvant_next, index2 in zip(my_list[index + 1:], range(index + 1, len(my_list))):
+            for el in rezolvant:
+                if opposite(el) in rezolvant_next:
+                    copy_rezolvant = rezolvant[:]
+                    copy_rezolvant.pop(copy_rezolvant.index(el))
+                    copy_rezolvant_next = rezolvant_next[:]
+                    copy_rezolvant_next.pop(copy_rezolvant_next.index(opposite(el)))
+                    new_rezolvant = sorted(list(set(copy_rezolvant + copy_rezolvant_next)))
+                    if new_rezolvant not in my_list:
+                        indexes_to_eliminate = [index, index2]
+                        doable = True
+                        break
+            if doable:
+                break
+        if doable:
+            break
+    if indexes_to_eliminate:
+        my_list.pop(indexes_to_eliminate[1])
+        my_list.pop(indexes_to_eliminate[0])
+        my_list = my_list + [new_rezolvant]
+        doable = True
+    return doable, my_list
 
 
 if __name__ == '__main__':
@@ -35,39 +127,42 @@ if __name__ == '__main__':
         if [] in my_list:
             message_to_show = "NU POATE FI SATISFACUT"
             break
-        doable = False
+        # doable = False
         pas += 1
         print("La pasul {} sirul este: {}".format(pas, my_list))
-        indexes_to_eliminate = []
-        new_list = []
-        for rezolvant, index in zip(my_list, range(len(my_list))):
-            for rezolvant_next, index2 in zip(my_list[index + 1:], range(index + 1, len(my_list))):
-                for el in rezolvant:
-                    if el[0:2] == 'n(' and break_not(el) in rezolvant_next:
-                        copy_rezolvant = rezolvant[:]
-                        copy_rezolvant.pop(copy_rezolvant.index(el))
-                        copy_rezolvant_next = rezolvant_next[:]
-                        copy_rezolvant_next.pop(copy_rezolvant_next.index(break_not(el)))
-                        new_list.append(list(set(copy_rezolvant + copy_rezolvant_next)))
-                        indexes_to_eliminate.append(index)
-                        indexes_to_eliminate.append(index2)
-                        doable = True
-                    elif create_not(el) in rezolvant_next:
-                        copy_rezolvant = rezolvant[:]
-                        copy_rezolvant.pop(copy_rezolvant.index(el))
-                        copy_rezolvant_next = rezolvant_next[:]
-                        copy_rezolvant_next.pop(copy_rezolvant_next.index(create_not(el)))
-                        new_list.append(list(set(copy_rezolvant + copy_rezolvant_next)))
-                        indexes_to_eliminate.append(index)
-                        indexes_to_eliminate.append(index2)
-                        doable = True
-        indexes_to_eliminate = list(set(indexes_to_eliminate))
-        indexes_to_eliminate = indexes_to_eliminate[::-1]
-        for index in indexes_to_eliminate:
-            my_list.pop(index)
-        my_list = my_list + new_list
+        doable, my_list = try_to_apply(my_list)
         if not doable:
             message_to_show = "SATISFACUT"
+        # indexes_to_eliminate = []
+        # new_list = []
+        # for rezolvant, index in zip(my_list, range(len(my_list))):
+        #     for rezolvant_next, index2 in zip(my_list[index + 1:], range(index + 1, len(my_list))):
+        #         for el in rezolvant:
+        #             if el[0:2] == 'n(' and break_not(el) in rezolvant_next:
+        #                 copy_rezolvant = rezolvant[:]
+        #                 copy_rezolvant.pop(copy_rezolvant.index(el))
+        #                 copy_rezolvant_next = rezolvant_next[:]
+        #                 copy_rezolvant_next.pop(copy_rezolvant_next.index(break_not(el)))
+        #                 new_list.append(list(set(copy_rezolvant + copy_rezolvant_next)))
+        #                 indexes_to_eliminate.append(index)
+        #                 indexes_to_eliminate.append(index2)
+        #                 doable = True
+        #             elif create_not(el) in rezolvant_next:
+        #                 copy_rezolvant = rezolvant[:]
+        #                 copy_rezolvant.pop(copy_rezolvant.index(el))
+        #                 copy_rezolvant_next = rezolvant_next[:]
+        #                 copy_rezolvant_next.pop(copy_rezolvant_next.index(create_not(el)))
+        #                 new_list.append(list(set(copy_rezolvant + copy_rezolvant_next)))
+        #                 indexes_to_eliminate.append(index)
+        #                 indexes_to_eliminate.append(index2)
+        #                 doable = True
+        # indexes_to_eliminate = list(set(indexes_to_eliminate))
+        # indexes_to_eliminate = indexes_to_eliminate[::-1]
+        # for index in indexes_to_eliminate:
+        #     my_list.pop(index)
+        # my_list = my_list + new_list
+        # if not doable:
+        #     message_to_show = "SATISFACUT"
     print("Sirul final: {}".format(my_list))
     with open("lab3_ex1_output.txt", "w") as file:
         file.write(message_to_show)
