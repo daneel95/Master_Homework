432880ead522e0174dc96392452adb9b31365da4
holteiu <holteiu@adobe.com>
2018-12-24 15:40:47 +0200

    [RCI] Lab4 rezolvare

---
+++ RCI/lab4_ex1.py
@@ -0,0 +1,67 @@
+def break_not(el):
+    # el de forma n(A)
+    return el[2:-1]
+
+
+def create_not(el):
+    # el de forma A
+    return '{}{}{}'.format('n(', el, ')')
+
+
+def opposite(el):
+    if el[0:2] == 'n(':
+        return break_not(el)
+    return create_not(el)
+
+
+def from_string_to_array(input):
+    list_to_return = []
+    input = input[1:-1]
+    while input.find('[') >= 0:
+        open = input.find('[')
+        close = input.find(']')
+        my_list = input[open + 1:close].replace(' ', '').split(',')
+        list_to_return.append(my_list)
+        input = input[close + 1:]
+    return list_to_return
+
+
+def from_string_to_array_atomic_prop(input):
+    return input[1:-1].replace(' ', '').split(',')
+
+
+def get_all_atoms(my_list):
+    atoms = []
+    for clause in my_list:
+        for atom in clause:
+            if atom[0:2] == 'n(':
+                atom_aux = opposite(atom)
+            else:
+                atom_aux = atom
+            if atom_aux not in atoms:
+                atoms.append(atom_aux)
+    return atoms
+
+
+def solve_inapoi(atoms, KB):
+    if len(atoms) == 0:
+        return "DA"
+    # print("UNUL NOU")
+    # print("Atoms: ", atoms)
+    # print("KB: ", KB)
+    for c in KB:
+        # print("C = ", c)
+        # print("Pentru Solve: ", [opposite(atom) for atom in c if atom != atoms[0]] + atoms[1:])
+        # print("==============================")
+        if atoms[0] in c and solve_inapoi([opposite(atom) for atom in c if atom != atoms[0]] + atoms[1:], KB) == "DA":
+            return "DA"
+    return "NU"
+
+
+if __name__ == "__main__":
+    # Laborator 4 ex1
+    with open("lab4_ex1_input.txt", "r") as file:
+        KB = from_string_to_array(file.readline())
+        propozitii_atomice = from_string_to_array_atomic_prop(file.readline())
+    with open("lab4_ex1_output.txt", "w") as file:
+        file.write(solve_inapoi(propozitii_atomice, KB))
