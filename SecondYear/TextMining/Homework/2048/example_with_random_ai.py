253bbd76541f9ff3f5a4981ba0325607994ac7e5
daneel95 <holteiu.daniel@gmail.com>
2020-05-15 18:23:04 +0300

    Reinforcement learning - cleared deleted and unused files

--- SecondYear/TextMining/Homework/2048/example_with_random_ai.py
+++
@@ -1,7 +0,0 @@
-from game_2048 import game_2048
-from random_game_ai import random_game_ai
-
-if __name__ == "__main__":
-    game = game_2048()
-    random_ai = random_game_ai()
-    random_ai.play_game(game)
--- SecondYear/TextMining/Homework/2048/main.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-    # Implementation comes here
-    pass
